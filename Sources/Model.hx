2694a2a58fc60cd5f1617e3fd85f36b8b6a26fbb
Artyom Lonsky <artyom.lonsky@gmail.com>
2019-01-29 16:39:18 +0300

    Refactor viewport

--- Sources/Model.hx
+++ Sources/Model.hx
@@ -1,4 +1,41 @@
+import kha.math.FastMatrix3;
+
 typedef Point = {x:Float, y:Float};
 typedef Segment = {p1:Point, p2:Point};
 typedef IntRange = {from:Int, to:Int};
 typedef FloatRange = {from:Float, to:Float};
+typedef Window = {w:Float, h:Float}
+typedef Viewport = {originalWindow:Window, targetWindow:Window, w:Float, h:Float, origin:Point, scaleX:Float, scaleY:Float, scaleMatrix:FastMatrix3}
+
+class ViewportExtensions {
+	public static function center(v:Viewport):Point {
+		return {x: v.origin.x + v.w / 2, y: v.origin.y + v.h / 2};
+	}
+
+	public static function applyZoom(v:Viewport, zoom:Float) {
+		var center = center(v);
+
+		v.w = v.targetWindow.w * zoom;
+		v.h = v.targetWindow.h * zoom;
+		v.scaleX = v.originalWindow.w / v.w;
+		v.scaleY = v.originalWindow.h / v.h;
+		v.origin.x = center.x - v.w / 2;
+		v.origin.y = center.y - v.h / 2;
+		v.scaleMatrix = FastMatrix3.scale(v.scaleX, v.scaleY);
+	}
+
+	public static function setup(original:Window, target:Window):Viewport {
+		var sX = original.w / target.w;
+		var sY = original.h / target.h;
+		return {
+			originalWindow: original,
+			targetWindow: target,
+			w: target.w,
+			h: target.h,
+			origin: {x: 0, y: 0},
+			scaleX: sX,
+			scaleY: sY,
+			scaleMatrix: FastMatrix3.scale(sX, sY)
+		};
+	}
+}
--- Sources/Simulation.hx
+++ Sources/Simulation.hx
@@ -1,3 +1,4 @@
+import Model.Window;
 import haxe.ds.Map;
 import kha.Image;
 import kha.input.Mouse;
@@ -16,11 +17,11 @@ import Settings;
 import galaxy.Galaxy;
 import GameControls;
 import render.Stars.StarRenderer;
+import Model.Viewport;
 
 using Utils.OptionExtensions;
 using Ellipse.EllipseExtensions;
-
-typedef Viewport = {w:Float, h:Float, centerX:Float, centerY:Float}
+using Model.ViewportExtensions;
 
 class Simulation {
 	var settings:Settings;
@@ -30,34 +31,24 @@ class Simulation {
 	var colMain = Color.White;
 	var galaxy:Galaxy;
 	//
-	var window = {w: 0.0, h: 0.0};
-	var scaledWindow = {w: 0.0, h: 0.0};
 	var viewport:Viewport;
-	var windowScale = FastMatrix3.identity();
 	var zoomLevel = 0;
 
 	public static inline var WIDTH = 320;
 	public static inline var HEIGHT = 240;
 
-	public function new(settings: Settings) {
+	public function new(settings:Settings) {
 		this.settings = settings;
 		gameControls = new GameControls(Mouse.get().toOption());
 		init();
 	}
 
 	function init() {
-		window = {w: System.windowWidth(0), h: System.windowHeight(0)};
-		scaledWindow = {w: WIDTH, h: HEIGHT};
-		windowScale = FastMatrix3.scale(window.w / WIDTH, window.h / HEIGHT);
-		viewport = {
-			w: Std.int(scaledWindow.w),
-			h: Std.int(scaledWindow.h),
-			centerX: Std.int(scaledWindow.w / 2),
-			centerY: Std.int(scaledWindow.h / 2)
-		};
+		var original:Window = {w: System.windowWidth(0), h: System.windowHeight(0)};
+		var target:Window = {w: WIDTH, h:HEIGHT};
 
-		var center = {x: scaledWindow.w / 2, y: scaledWindow.h / 2};
-		galaxy = new Galaxy(center, settings.coreRadius, settings.diskRadius, settings.farRadius, settings.starsCount);
+		viewport = ViewportExtensions.setup(original, target); 
+		galaxy = new Galaxy(viewport.center(), settings.coreRadius, settings.diskRadius, settings.farRadius, settings.starsCount);
 	}
 
 	public function update():Void {
@@ -73,18 +64,14 @@ class Simulation {
 			} else {
 				Math.pow(if (zoomLevel < 0) settings.zoomInFactor else 1 / settings.zoomInFactor, Math.abs(zoomLevel));
 			}
-			viewport.w = Std.int(WIDTH * zoom);
-			viewport.h = Std.int(HEIGHT * zoom);
-			windowScale = FastMatrix3.scale(window.w / viewport.w, window.h / viewport.h);
+			viewport.applyZoom(zoom);
 		});
 		gameControls.consumeDragEvent(function(e:DragEvent) {
 			switch (e) {
 				case DragMove(x, y, dx, dy):
 					{
-						var scaleX = viewport.w / window.w;
-						var scaleY = viewport.h / window.h;
-						viewport.centerX -= dx * scaleX;
-						viewport.centerY -= dy * scaleY;
+						viewport.origin.x -= dx / viewport.scaleX;
+						viewport.origin.y -= dy / viewport.scaleY;
 					}
 				default:
 			}
@@ -96,12 +83,8 @@ class Simulation {
 		g.begin();
 		g.clear(colBg);
 
-		var scaleX = window.w / viewport.w;
-		var scaleY = window.h / viewport.h;
-		var viewportOrigin = {x: viewport.centerX - viewport.w / 2, y: viewport.centerY - viewport.h / 2};
-
-		g.pushTransformation(windowScale);
-		g.pushTranslation(-viewportOrigin.x * scaleX, -viewportOrigin.y * scaleY);
+		g.pushTransformation(viewport.scaleMatrix);
+		g.pushTranslation(-viewport.origin.x * viewport.scaleX, -viewport.origin.y * viewport.scaleY);
 
 		g.color = colMain;
 
@@ -121,20 +104,13 @@ class Simulation {
 	}
 
 	var haloBank:Map<Int, Image> = [];
-	var haloTemplate : Image = null;
+	var haloTemplate:Image = null;
 
 	function drawStar(star:Star, g:kha.graphics2.Graphics) {
 		var starRadius = settings.sunRadius * star.radius;
 		var starColor = StarRenderer.getColor(star.temperature);
 		g.color = starColor;
-		// g.drawRect(star.center.x - starRadius, star.center.y - starRadius, starRadius * 2, starRadius * 2, starRadius * 2);
 
-		// g.color = Color.White;
-		// var halo = haloBank[starColor];
-		// if (halo == null) {
-		// 	halo = StarRenderer.getHalo(star);
-		// 	haloBank[starColor] = halo;
-		// }
 		var halo = haloTemplate;
 		if (halo == null) {
 			haloTemplate = StarRenderer.getHalo(star);
